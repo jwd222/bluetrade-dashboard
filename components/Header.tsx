db247642603e09baa55c72382e09ae2065b59b04
jwd222 <sji.2.sami@gmail.com>
2023-09-12 20:54:59 +0600

    basic layout completed with header and sidebar

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Dashboard</h2>
+      <h2>Welcome, back</h2>
+    </div>
+  )
+}
+export default Header
