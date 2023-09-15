fe5c49d6dd54955db483465d2fe06db90c190a35
jwd222 <sji.2.sami@gmail.com>
2023-09-16 00:01:03 +0600

    edited theme switcher icon

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -8,8 +8,8 @@ const ThemeSwitch = () => {
   return (
     <button
       className="bg-white 
-    w-[3rem] h-[3rem] bg-opacity-80 backdrop:blur-[0.5rem] border
-    border-white border-opacity-40 shadow-2xl rounded-full
+    w-[2.5rem] h-[2.5rem] bg-opacity-80 backdrop:blur-[0.5rem] border
+    border-black/5 dark:border-white/10 shadow-2xl rounded-full
      flex items-center justify-center hover:scale-[1.15] 
      active:scale-105 transition-all dark:bg-gray-950 dark:text-white"
       onClick={toggleTheme}
