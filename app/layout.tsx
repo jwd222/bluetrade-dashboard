1a45f14c71692858e58c3b707131a4bc4db6cca1
jwd222 <sji.2.sami@gmail.com>
2023-09-15 17:45:24 +0600

    changed global css file location

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/Header'
-import './globals.css'
+import '../styles/globals.css'
 import { Inter } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
 
