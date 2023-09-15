934f24b299b95de723a14dfbe72dc745de6ca1ba
jwd222 <sji.2.sami@gmail.com>
2023-09-16 03:34:14 +0600

    added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Asset from '@/components/Asset'
 import Cards from '@/components/Cards'
 import Chart from '@/components/Chart'
+import Converter from '@/components/Coverter'
 import Header from '@/components/Header'
+import History from '@/components/History'
 import Market from '@/components/Market'
 
 export default function Home() {
@@ -8,9 +11,14 @@ export default function Home() {
     <div className="flex flex-col min-h-screen w-full">
       <Header />
       <Cards />
-      <div className="p-4 flex sm:flex-row flex-col gap-2 ml-2 ">
+      <div className="p-4 pt-1 flex sm:flex-row flex-col gap-2 ml-2">
         <Chart />
         <Market />
+      </div>
+      <div className="p-4 pt-1 flex sm:flex-row flex-col gap-2 ml-2">
+        <Asset />
+        <Converter />
+        <History />
       </div>
     </div>
   )
