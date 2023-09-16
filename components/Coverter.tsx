3ad21a6ba2d3088cdf0db619ab853d24dd5fed76
jwd222 <sji.2.sami@gmail.com>
2023-09-16 22:17:46 +0600

    converter completed

--- components/Coverter.tsx
+++ components/Coverter.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
@@ -8,8 +10,49 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { useState } from 'react'
 
 const Converter = () => {
+  const [convertFrom, setConvertFrom] = useState<number>(0)
+  const [convertTo, setConvertTo] = useState<number>(0)
+
+  const [fromSelect, setFromSelect] = useState<string | ''>('USDT')
+  const [toSelect, setToSelect] = useState<string | ''>('BTC')
+
+  const fromHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(convertFrom)
+    setConvertFrom(Number(e.target.value))
+  }
+  const toHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(convertTo)
+    setConvertTo(Number(e.target.value))
+  }
+
+  const handleConvert = () => {
+    if (convertFrom !== undefined) {
+      if (fromSelect === toSelect) setConvertTo(convertFrom)
+      else if (fromSelect === 'USDT') {
+        if (toSelect === 'BTC') {
+          setConvertTo(convertFrom / 26053)
+        } else if (toSelect === 'BDT') {
+          setConvertTo(convertFrom * 109.67)
+        }
+      } else if (fromSelect === 'BDT') {
+        if (toSelect === 'BTC') {
+          setConvertTo(convertFrom / (26053 * 109.67))
+        } else if (toSelect === 'USDT') {
+          setConvertTo(convertFrom / 109.67)
+        }
+      } else if (fromSelect === 'BTC') {
+        if (toSelect === 'USDT') {
+          setConvertTo(convertFrom * 26053)
+        } else if (toSelect === 'BDT') {
+          setConvertTo(convertFrom * (109.67 * 26053))
+        }
+      }
+    }
+  }
+
   return (
     <div
       className="flex flex-col items-start bg-white 
@@ -27,8 +70,14 @@ const Converter = () => {
         </Button>
       </div>
       <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input className="dark:bg-black border-white/10" type="text" />
-        <Select>
+        <Input
+          className="dark:bg-black border dark:border-white/30 
+          border-black/30 dark:text-white"
+          type="number"
+          // value={convertFrom}
+          onChange={fromHandleChange}
+        />
+        <Select value={fromSelect} onValueChange={setFromSelect}>
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Currency" />
           </SelectTrigger>
@@ -41,14 +90,26 @@ const Converter = () => {
       </div>
       <div className="flex w-full max-w-sm items-center space-x-2">
         <Input
-          className="dark:bg-black border dark:border-white/10 
-          border-black/5"
+          className="dark:bg-black border dark:border-white/30 
+          border-black/30 dark:text-white"
           type="text"
-          placeholder=""
+          value={convertTo}
+          onChange={toHandleChange}
         />
-        <Button type="button">Subscribe</Button>
+        <Select value={toSelect} onValueChange={setToSelect}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue className="dark:text-white" placeholder="USDT" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="USDT">USDT</SelectItem>
+            <SelectItem value="BDT">BDT</SelectItem>
+            <SelectItem value="BTC">BTC</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
-      <Button className="w-full ">Convert now</Button>
+      <Button className="w-full" onClick={handleConvert}>
+        Convert now
+      </Button>
       <p className=" p-0 text-xs dark:text-white">
         The ultimate price and output is determined by the amount of tokens in
         the pool at the time of your swap.
