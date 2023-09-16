3bae28b62db98539baffcb870df098b1474d0746
jwd222 <sji.2.sami@gmail.com>
2023-09-16 23:20:22 +0600

    history completed

--- components/History.tsx
+++ components/History.tsx
@@ -1,14 +1,15 @@
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
 import { Button } from './ui/button'
 import { historyData } from '@/lib/data'
+import { BiUpArrowAlt, BiDownArrowAlt } from 'react-icons/bi'
 
 const History = () => {
   return (
     <div
       className="rounded-lg flex flex-col shadow basis-2/4
-    dark:text-white dark:bg-gray-900 bg-white p-8"
+    dark:text-white dark:bg-gray-900 bg-white px-2 pt-2"
     >
-      <div className="flex flex-row justify-between w-full">
+      <div className="flex flex-row justify-between w-full px-4 pt-2">
         <h1 className="text-lg dark:text-white justify-start">History</h1>
         <Button
           className="dark:bg-gray-950 dark:text-white/30 text-xs 
@@ -21,14 +22,27 @@ const History = () => {
       </div>
       <Table>
         <TableBody>
-          {historyData.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">
-                {invoice.totalAmount}
+          {historyData.map(({ type, name, value, time, status }, index) => (
+            <TableRow key={index}>
+              <TableCell
+                className={`${
+                  type === 'in'
+                    ? 'text-green-500 bg-green-400/10'
+                    : 'text-red-500 bg-red-400/10'
+                } rounded-full scale-50 text-[2.5rem] flex justify-center`}
+              >
+                {type === 'in' ? <BiDownArrowAlt /> : <BiUpArrowAlt />}
               </TableCell>
+              <TableCell>{name}</TableCell>
+              <TableCell
+                className={`${
+                  value > 0 ? 'text-green-500' : 'text-red-500'
+                } text-center`}
+              >
+                {`${value > 0 ? `+$${value}` : `-$${value}`}`}
+              </TableCell>
+              <TableCell>{time}</TableCell>
+              <TableCell className="text-center">{status}</TableCell>
             </TableRow>
           ))}
         </TableBody>
