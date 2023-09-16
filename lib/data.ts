f8aa92f338a68a4cb9b20cafc93ac76b4db68726
jwd222 <sji.2.sami@gmail.com>
2023-09-16 22:18:03 +0600

    added a;; data to data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -73,3 +73,189 @@ export const sideBarButtons = [
     icon: React.createElement(IoIosSettings),
   },
 ] as const
+
+export const chartData = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+]
+
+export const historyData = [
+  {
+    invoice: 'INV001',
+    paymentStatus: 'Paid',
+    totalAmount: '$250.00',
+    paymentMethod: 'Credit Card',
+  },
+  {
+    invoice: 'INV002',
+    paymentStatus: 'Pending',
+    totalAmount: '$150.00',
+    paymentMethod: 'PayPal',
+  },
+  {
+    invoice: 'INV003',
+    paymentStatus: 'Unpaid',
+    totalAmount: '$350.00',
+    paymentMethod: 'Bank Transfer',
+  },
+  {
+    invoice: 'INV004',
+    paymentStatus: 'Paid',
+    totalAmount: '$450.00',
+    paymentMethod: 'Credit Card',
+  },
+  {
+    invoice: 'INV005',
+    paymentStatus: 'Paid',
+    totalAmount: '$550.00',
+    paymentMethod: 'PayPal',
+  },
+  {
+    invoice: 'INV006',
+    paymentStatus: 'Pending',
+    totalAmount: '$200.00',
+    paymentMethod: 'Bank Transfer',
+  },
+] as const
+
+export const marketData = [
+  {
+    currToUsdt: 'BTCUSDT',
+    currencyName: 'Bitcoin',
+    currentValue: ' $26,053',
+    percentage: +23.4,
+  },
+  {
+    currToUsdt: 'ETHUSDT',
+    currencyName: 'Ethereum',
+    currentValue: '$1670',
+    percentage: -0.3,
+  },
+  {
+    currToUsdt: 'DOTUSDT',
+    currencyName: 'Polkadot',
+    currentValue: '$4.46',
+    percentage: +0.5,
+  },
+  {
+    currToUsdt: 'SOLUSDT',
+    currencyName: 'Solana',
+    currentValue: '$21.18',
+    percentage: +11.1,
+  },
+  {
+    currToUsdt: 'BNBUSDT',
+    currencyName: 'Bianance',
+    currentValue: '$209.13',
+    percentage: -2.6,
+  },
+  {
+    currToUsdt: 'ADAUSDT',
+    currencyName: 'Cardano',
+    currentValue: '$0.2666',
+    percentage: -1.03,
+  },
+  {
+    currToUsdt: 'TRXUSDT',
+    currencyName: 'TRON',
+    currentValue: '$0.07469',
+    percentage: +6.3,
+  },
+  {
+    currToUsdt: 'XRPUSDT',
+    currencyName: 'Ripple',
+    currentValue: '$0.5246',
+    percentage: +0.11,
+  },
+  {
+    currToUsdt: 'DAIUSDT',
+    currencyName: 'Dai',
+    currentValue: '$0.9998',
+    percentage: +0.41,
+  },
+  {
+    currToUsdt: 'DOGEUSDT',
+    currencyName: 'Dogecoin',
+    currentValue: '$0.06248',
+    percentage: -5.16,
+  },
+] as const
+
+export const tinyChartData = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+]
