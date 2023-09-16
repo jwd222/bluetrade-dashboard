c367b05f546799de91f13814b2277819d670a81c
jwd222 <sji.2.sami@gmail.com>
2023-09-16 23:20:54 +0600

    custom function to get date for history

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formattedDate(milliseconds: number): string {
+  const date = new Date(milliseconds)
+
+  const options = {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  } as const
+
+  const formatter = new Intl.DateTimeFormat('en-US', options)
+  return formatter.format(date)
+}
